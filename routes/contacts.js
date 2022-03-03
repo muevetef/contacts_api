f6a79fe6a076152f2fc299935034a06319d3b85e
Antoni Moragues Font <easyrobotics2@gmail.com>
2022-03-03 14:11:39 +0100

    auth middleware

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,15 +1,58 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
+const Contact = require("../models/Contact");
+const User = require("../models/User");
 const router = express.Router();
-
+const auth = require("../middleware/auth");
 //@route GET api/contacts
 //@desc Obtener todos los contactos
 //@access Private
-router.get("/", (req, res) => res.json({ contacts: "Array con contactos" }));
+router.get("/", auth, (req, res) => {
+  try {
+    res.json({
+      msg: "No tienes contactos",
+      user: req.user
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Error en el servidor");
+  }
+});
 
 //@route POST api/contacts
 //@desc  Crear un contacto nuevo
 //@access Private
-router.post("/", (req, res) => res.json({ contact: "EL contacto creado" }));
+router.post(
+  "/",
+  [
+    check("name", "El nombre es requerido").not().isEmpty(),
+    check("type", "Tiene que ser Personal o Profesional").isIn([
+      "Personal",
+      "Profesional",
+    ]),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors);
+    }
+    try {
+      const { name, email, phone, type } = req.body;
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+      });
+
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Error en el servidor");
+    }
+  }
+);
 
 //@route PUT api/contacts/:id
 //@desc Actualiza un contacto
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
 
@@ -9,40 +11,57 @@ const router = express.Router();
 //@route  POST api/users
 //@desc   Registra un nuevo usuario
 //@access  Public
-router.post("/", async (req, res) => {
-  //Validar los datos
-  //Mirar si el User ya existe
-  const { name, email, password } = req.body;
-  try {
-    let user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({ msg: "EL usuario ya existe" });
+router.post(
+  "/",
+  [
+    check("name", "El nombre es requerido").not().isEmpty(),
+    check("email", "Por favor incluye un email valido").isEmail(),
+    check("password", "El password debe tener mas de 6 cracteres").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors);
     }
+    //Mirar si el User ya existe
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: "EL usuario ya existe" });
+      }
 
-    user = new User({
-      name,
-      email,
-      password,
-    });
+      user = new User({
+        name,
+        email,
+        password,
+      });
 
-    //Encriptar el password
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(password, salt);
+      //Encriptar el password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
 
-    await user.save();
+      await user.save();
 
-    //Generamos un token JWT y lo enviamos
-    const payload = { user: { id: user.id } };
+      //Generamos un token JWT y lo enviamos
+      const payload = { user: { id: user.id } };
 
-    jwt.sign(payload,'secreto',{expiresIn: 3600}, (err, token) => {
-      if(err) throw err;
-      res.json({token});
-    });
-    
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Error en el servidor");
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Error en el servidor");
+    }
   }
-});
+);
 
 module.exports = router;
